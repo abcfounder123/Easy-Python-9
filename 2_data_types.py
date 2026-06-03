9954a9cb1c50833e68ef3007929bd17c272bc118
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-06-03 23:32:02 +0700

    data types 15

---
+++ 2_data_types.py
@@ -0,0 +1,24 @@
+
+"""
+
+Data Types (15)
+
+စာ သုံးခု
+1. Character              --->  chr()
+2. Character String       --->  str()
+3. Documentation String   --->  doc
+
+နံပါတ် သုံးခု
+1. Integer                --->  int()
+2. Floating-point number  --->  float()
+3. Complex number         --->  complex()
+
+Data types 15 မျိုး
+စာ သုံးခု
+နံပါတ် သုံးခု
+စာရင်း ရှစ်ခု
+အခြေအနေ တစ်ခု
+
+#################################################
+
+"""
