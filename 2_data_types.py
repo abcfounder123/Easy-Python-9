ddfabbd0aac0149792bff93450b3332d3e86a02d
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-06-04 22:36:23 +0700

    data types 15

--- 2_data_types.py
+++ 2_data_types.py
@@ -13,6 +13,16 @@ Data Types (15)
 2. Floating-point number  --->  float()
 3. Complex number         --->  complex()
 
+စာရင်း ရှစ်ခု
+1. Normal list            --->  list(), tuple()
+2. Unique list            --->  set(), frozenset()
+3. Binary list            --->  bytearray(), bytes()
+4. Number                 --->  range()
+5. Item list              --->  dict()
+
+အခြေအနေ တစ်ခု
+1. boolean data type      --->  bool()
+
 Data types 15 မျိုး
 စာ သုံးခု
 နံပါတ် သုံးခု
@@ -20,5 +30,111 @@ Data types 15 မျိုး
 အခြေအနေ တစ်ခု
 
 #################################################
+
+Exercises
+
+1 to 99    =>  range(1, 100, 1)
+start = 1
+stop = 100
+step = 1
+
+1 to 100    =>  range(1, 101, 1)
+start = 1
+stop = 101
+step = 1
+
+2, 4, 6, ... to 100    =>  range(2, 101, 2)
+start = 2
+stop = 101
+step = 2
+
+1, 3, 5, ... to 99    =>  range(1, 100, 2)
+start = 1
+stop = 100
+step = 2
+
+100, 98, 96, ... to 2    =>  range(100, 1, -2)
+start = 100
+stop = 1
+step = -2
+
+99, 97, 95, ... to 1    =>  range(99, 0, -2)
+start = 99
+stop = 0
+step = -2
+
+-100, -98, -96, ... to  -2    =>  range(-100, -1, 2)
+start = -100
+stop = -1
+step = 2
+
+-2, -4, -6, ... to  -100    =>  range(-2, -101, -2)
+start = -2
+stop = -101
+step = -2
+
+
+255, 260, ..., 500
+range(255, 501, 5)
+
+500, 495, ..., 255
+range(500, 254, -5)
+
+0 to 10    =>  range(0, 11, 1)
+start = 0
+stop = 11
+step = 1
+
+range(0, 11, 1)
+range(0, 11)    <--- step = 1
+range(11)       <--- start = 0, step = 1
+
+#################################################
+
+Data types of Python (15)
+
+1. str()
+2. int()
+3. float()
+4. complex()
+5. list()
+6. tuple()
+7. set()
+8. frozenset()
+9. bytearray()
+10. bytes()
+11. range()
+12. dict()
+13. bool()
+
+14. memoryview()
+15. NoneTypes
+
+#################################################
+
+Creating data types by name (14)
+
+1. str()
+2. int()
+3. float()
+4. complex()
+5. list()
+6. tuple()
+7. set()
+8. frozenset()
+9. bytearray()
+10. bytes()
+11. range()
+12. dict()
+13. bool()
+14. memoryview()
+
+#################################################
+
+1 + 2                     <--- Creating data types by symbols
+int(1).__add__(int(2))    <--- Creating data types by name
+
+#################################################
+
 
 """
