9473d6d061838d0da53934fd3727e15db10569f8
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-07-08 22:49:38 +0700

    loop

---
+++ day_10_loop.py
@@ -0,0 +1,48 @@
+
+"""
+
+3. Loop
+   1. Iteration - value  ( meaningful )
+   2. Looping - 12 times ( underscore )
+   3. nested loop
+   4. definite loop (for)
+   5. indefinite loop (while)
+   6. break => to stop loop
+   7. exit()  => to stop program
+   8. else  => if, for, while
+
+##########################################
+
+1. Iteration
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]   
+
+for number in numbers:
+    print(f"2 x {number} = {2 * number}")
+
+for r in range(1, 13, 1):
+    print(f"2 x {r} = {2 * r}")
+
+##########################################
+
+2. Looping - 12 times   (definite loop)
+
+for _ in range(12):
+    print("apple")
+
+##########################################
+
+for n in range(1, 11):
+    print(f"{n}.I love you.")
+
+for _ in range(10):
+    print("I love you.")
+
+
+for n in range(1, 11):    =>  iteration =>  1, ..., 10
+for _ in range(10):       =>  loop      =>  10
+
+##########################################
+
+
+"""
